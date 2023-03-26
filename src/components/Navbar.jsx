959c89bb3f0b70535a56086883f9dd5e18689863
Bilal Atique <74999164+BilalAtique@users.noreply.github.com>
2023-03-26 19:47:55 +0500

    Implment Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <span className="logo">Swirl Chat</span>
+      <div className="user">
+        <img src="https://images.pexels.com/photos/14737533/pexels-photo-14737533.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+        <span>John</span>
+        <button>Logout</button>
+      </div>
+    </div>
+
+  )
+}
+
+export default Navbar
