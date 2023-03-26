c54d52674d4e5b28d4769269b2610d9586e83ff7
Bilal Atique <74999164+BilalAtique@users.noreply.github.com>
2023-03-26 07:48:13 +0500

    Implement Register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.scss'
 import Register from './pages/Register'
 
 function App() {
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,26 @@
-const Register = () => {
-  return (
-    <div className="formContainer">
-        <div className="formWrapper">
-            Register
-        </div>
-    </div>
-  )
-}
+import AddAvatarIcon from "../assets/add-avatar.png";
 
-export default Register
+const Register = () => {
+    return (
+        <div className="formContainer">
+            <div className="formWrapper">
+                <span className="logo">Swirl Chat</span>
+                <span className="title">Register</span>
+                <form>
+                    <input type="text" placeholder="Diplay Name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <input type="file" id="file" />
+                    <label htmlFor="file">
+                        <img src={AddAvatarIcon} />
+                        <span>Add an Avatar</span>
+                    </label>
+                    <button>Sign up</button>
+                </form>
+                <p>Do you already have an account? Login</p>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
